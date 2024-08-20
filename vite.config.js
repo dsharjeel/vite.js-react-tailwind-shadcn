d0d516163c4c18e4c74eb0e5ac4ffb2c637cc3bc
dsharjeel <danishsharjeel7@gmail.com>
2024-08-20 09:57:59 +0500

    init

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import path from "path";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import react from "@vitejs/plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  }
+});
